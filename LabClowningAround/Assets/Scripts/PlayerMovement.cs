40fed0a85599feec214fa93d98d6cdb3314ad30f
Devvy <18744053+KaiDevvy@users.noreply.github.com>
2024-01-27 23:15:46 +0000

    Modified player movement to fix clipping issues

--- LabClowningAround/Assets/Scripts/PlayerMovement.cs
+++ LabClowningAround/Assets/Scripts/PlayerMovement.cs
@@ -4,98 +4,76 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerMovement : MonoBehaviour
 {
-
-    //Input variables 
+    public float moveSpeed = 10f;
+    public float jumpForce = 10;
+    public float acceleration = 1.0f;
+    public float deceleration = 1.0f;
+    public float groundDistance = 0.1f;
+    
     private InputAction moveInput;
-    private InputAction fireInput;
     private InputAction jumpInput;
-    [SerializeField] private float moveSpeed = 10f;
-    private Vector3 moveDirection = Vector3.zero;
-    private Vector3 lookDirection = Vector3.forward;
-    private float jumping = 0;
     public Rigidbody2D rb;
-    private bool isJumping = false;
 
+    private Vector3 moveDirection = Vector3.zero;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
+        PlayerInput inputSystem = GetComponent<PlayerInput>();
+        moveInput = inputSystem.actions.FindAction("Move");
+        jumpInput = inputSystem.actions.FindAction("Jump");
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Use the moveDirection to move
-        Vector3 movement = new Vector3(moveDirection.x, 0f, moveDirection.z);
-        movement *= moveSpeed * Time.deltaTime;
-
-        transform.Translate(movement, Space.World);
-
-        //lock player rotation
-        transform.rotation = Quaternion.identity;
-
-    }
-
-    private void OnEnable()
-    {
-        moveInput = GetComponent<PlayerInput>().actions.FindAction("Move");
-        fireInput = GetComponent<PlayerInput>().actions.FindAction("Fire");
-        jumpInput = GetComponent<PlayerInput>().actions.FindAction("Jump");
-
-        // Subscribe to the move input event
-        moveInput.performed += OnMovePerformed;
-        moveInput.canceled += OnMoveCanceled;
-
+        moveInput.performed += OnMoveUpdated;
         jumpInput.performed += OnJumpPerformed;
+    }
+    
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float desiredVelocity = moveSpeed * moveDirection.x;
+        float differenceVelocity = desiredVelocity - rb.velocity.x;
 
+        // If horizontal input is 0 decelerate instead of accelerate
+        // floats are bad at == 0 so we check if it's below minimum threshold (epsilon)
+        float scale = Mathf.Abs(moveDirection.x) < float.Epsilon ? deceleration : acceleration;
+        
+
+        rb.AddForce(differenceVelocity * scale * Vector2.right, ForceMode2D.Force);
     }
 
-    private void OnDisable()
+    private void OnDestroy()
     {
         // Unsubscribe from the move input event
-        moveInput.performed -= OnMovePerformed;
-        moveInput.canceled -= OnMoveCanceled;
+        moveInput.performed -= OnMoveUpdated;
+        moveInput.canceled -= OnMoveUpdated;
+        jumpInput.performed -= OnJumpPerformed;
     }
-    private void OnMovePerformed(InputAction.CallbackContext context)
+    private void OnMoveUpdated(InputAction.CallbackContext context)
     {
         // Read the move input
         moveDirection = context.ReadValue<Vector3>();
     }
-
-    private void OnMoveCanceled(InputAction.CallbackContext context)
-    {
-        // Reset the move input when it's canceled
-        moveDirection = Vector3.zero;
-    }
+    
 
     private void OnJumpPerformed(InputAction.CallbackContext context)
     {
-        if(!isJumping)
-        {
-            isJumping = true;
-            rb.AddForce(new Vector2(0f, context.ReadValue<float>()), ForceMode2D.Impulse);
-            //StartCoroutine(ResestJump());
-        }
+        if (!CanJump()) 
+            return;
+        
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
-    private IEnumerator ResestJump()
-    {
-        yield return new WaitForSeconds(2f);
-        isJumping = false;
-    }
+    // Checks if the player is raising/falling and that there's floor beneath them
+    private bool CanJump() =>
+        rb.velocity.y < 0.01f &&
+        Physics2D.Raycast(transform.position, Vector2.down, groundDistance);
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(isJumping)
-        {
-            if(collision.collider.tag =="Ground" )
-            {
-                isJumping = false;
-            }
 
-        }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, Vector2.down * groundDistance);
     }
 }
