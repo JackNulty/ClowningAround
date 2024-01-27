d5f650e17ef4e4929c8fc01b0865d50231e428bd
JackNulty <jdnulty19@gmail.com>
2024-01-27 20:20:00 +0000

    more interactions and menu

---
+++ LabClowningAround/Assets/Scripts/ButtonManagerScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonManagerScript : MonoBehaviour
+{
+    public Button play;
+    public Button LevelSelect;
+    public Button Quit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        play = GetComponent<Button>();
+        LevelSelect = GetComponent<Button>();   
+        Quit = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPlayPress()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void OnQuitPress()
+    {
+        Application.Quit();
+    }
+    public void ONlevelSelectPress()
+    {
+
+    }
+}
