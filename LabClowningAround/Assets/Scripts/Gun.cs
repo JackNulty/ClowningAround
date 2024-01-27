86ab140a3d969654033d2e396b4c4980b379457b
Devvy <18744053+KaiDevvy@users.noreply.github.com>
2024-01-27 22:47:05 +0000

    Completed Gun

--- LabClowningAround/Assets/Scripts/Gun.cs
+++ LabClowningAround/Assets/Scripts/Gun.cs
@@ -9,12 +9,27 @@ public class Gun : MonoBehaviour
     public float range = 5f;
     public float angle = 10f;
 
+    private ParticleSystem vfx;
+
+    private void Awake()
+    {
+        vfx = GetComponentInChildren<ParticleSystem>();
+        if (vfx == null)
+        {
+            Debug.LogError("[Gun] No particle system found in children");
+        }
+    }
 
     public void OnFire()
     {
+        /*
+         * Cast cone in direction of player mouse
+         */
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 
         float pointedAngle = (mousePos - transform.position).ToAngle();
+        vfx.transform.rotation = Quaternion.Euler(0, 0, pointedAngle);
+        vfx.Play();
 
         foreach (HahaMover instance in HahaMover.all)
         {
@@ -27,9 +42,9 @@ public class Gun : MonoBehaviour
         }
     }
 
-    private void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.red;
+        Gizmos.color = Color.red * new Color(1,1,1,0.1f);
         Gizmos.DrawWireSphere(transform.position, range);
 
 
