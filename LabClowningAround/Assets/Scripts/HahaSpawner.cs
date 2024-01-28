b3d23d74b23844b56295dd0fff29c5058234d7e6
Devvy <18744053+KaiDevvy@users.noreply.github.com>
2024-01-28 12:53:33 +0000

    Lips Laugh & Haha force

--- LabClowningAround/Assets/Scripts/HahaSpawner.cs
+++ LabClowningAround/Assets/Scripts/HahaSpawner.cs
@@ -8,14 +8,23 @@ using UnityEngine;
 
 public class HahaSpawner : MonoBehaviour
 {
+    public Animator laughTrigger;
     public static List<HahaSpawner> all = new List<HahaSpawner>();
+    public float upwardForce = 5f;
 
     public void TriggerSpawn(Vector2 initialVelocity)
     {
         GameObject haha = GameManager.instance.resources.GetPrefab("hahatest");
         GameObject instance = Instantiate(haha, transform.position, Quaternion.identity);
 
+        initialVelocity.y += upwardForce;
+
         instance.GetComponent<Rigidbody2D>().AddForce(initialVelocity, ForceMode2D.Impulse);
+
+        if (laughTrigger == null)
+            return;
+
+        laughTrigger.SetTrigger("Laugh");
     }
 
     private void Awake()
